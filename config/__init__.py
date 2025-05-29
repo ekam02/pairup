dafece52f551b38aab7293b38db76eccd46a902c
Enrique Martínez Agudelo <enrique.martinezagudelo@externos-co.cencosud.com>
2025-05-29 18:46:16 -0500

    refactor: reacondiciona configuraciones base
    Reacondiciona los módulos "__init__.py" y "settings.py"

---
+++ config/__init__.py
@@ -0,0 +1,8 @@
+from .settings import settings
+fact_engine = settings.fact_engine
+jano_engine = settings.jano_engine
+OUTPUT_DIR = settings.OUTPUT_DIR
+REPORT_NAME = settings.REPORT_NAME
+STORES = settings.STORES
+START_DATE = settings.START_DATE
+END_DATE = settings.END_DATE
--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import logging
 from os import getenv
 from pathlib import Path
 
@@ -12,45 +13,68 @@ except ModuleNotFoundError:
     import tomli as tomllib
 
 
-init_oracle_client()
-# load_dotenv()
-if load_dotenv():
-    F_REP_USER = getenv('F_REP_USER')
-    F_REP_PASS = getenv('F_REP_PASS')
-    F_REP_HOST = getenv('F_REP_HOST')
-    F_REP_PORT = getenv('F_REP_PORT')
-    F_REP_NAME = getenv('F_REP_NAME')
-    F_REP_SCHE = getenv('F_REP_SCHE')
-    J_PRE_USER = getenv('J_PRE_USER')
-    J_PRE_PASS = getenv('J_PRE_PASS')
-    J_PRE_HOST = getenv('J_PRE_HOST')
-    J_PRE_PORT = getenv('J_PRE_PORT')
-    J_PRE_NAME = getenv('J_PRE_NAME')
-else:
-    raise Exception('No .env file found')
+logger = logging.getLogger(__name__)
 
 
-fact_engine = create_engine(
-    f'postgresql://{F_REP_USER}:{F_REP_PASS}@{F_REP_HOST}:{F_REP_PORT}/{F_REP_NAME}',
-    pool_size=5, max_overflow=10,
-    connect_args={'options': f'-csearch_path={F_REP_SCHE}'}
-)
-jano_engine = create_engine(
-    f'oracle+oracledb://{J_PRE_USER}:{J_PRE_PASS}@{J_PRE_HOST}:{J_PRE_PORT}/{J_PRE_NAME}',
-    pool_size=5, max_overflow=10
-)
+class AppSettings:
+    _instance = None  # Patrón Singleton para asegurar una única instancia
 
-try:
-    config_file = Path(__file__).parent / 'config.toml'
-    with config_file.open('rb') as config_file:
-        cfg = tomllib.load(config_file)
-        DATABASE_NAME = cfg['DATABASE_NAME']
-        OUTPUT_DIR = Path(cfg['OUTPUT_DIR'])
-        REPORT_NAME = cfg['REPORT_NAME']
-        STORES = ', '.join([str(st) for st in cfg['STORES']])
-        START_DATE = cfg['DATETIME']['START_DATE']
-        END_DATE = cfg['DATETIME']['END_DATE']
-except FileNotFoundError:
-    pass
-except Exception as e:
-    pass
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(AppSettings, cls).__new__(cls)
+            cls._instance._load_settings()
+        return cls._instance
+
+    def _load_settings(self):
+        init_oracle_client()
+        if load_dotenv():
+            self.F_REP_USER = getenv('F_REP_USER')
+            self.F_REP_PASS = getenv('F_REP_PASS')
+            self.F_REP_HOST = getenv('F_REP_HOST')
+            self.F_REP_PORT = getenv('F_REP_PORT')
+            self.F_REP_NAME = getenv('F_REP_NAME')
+            self.F_REP_SCHE = getenv('F_REP_SCHE')
+            self.J_PRE_USER = getenv('J_PRE_USER')
+            self.J_PRE_PASS = getenv('J_PRE_PASS')
+            self.J_PRE_HOST = getenv('J_PRE_HOST')
+            self.J_PRE_PORT = getenv('J_PRE_PORT')
+            self.J_PRE_NAME = getenv('J_PRE_NAME')
+            logger.debug('Environment variables loaded')
+
+            self.fact_engine = create_engine(
+                f'postgresql://{self.F_REP_USER}:{self.F_REP_PASS}@{self.F_REP_HOST}:{self.F_REP_PORT}/{self.F_REP_NAME}',
+                pool_size=5, max_overflow=10,
+                connect_args={'options': f'-csearch_path={self.F_REP_SCHE}'}
+            )
+            self.jano_engine = create_engine(
+                f'oracle+oracledb://{self.J_PRE_USER}:{self.J_PRE_PASS}@{self.J_PRE_HOST}:{self.J_PRE_PORT}/{self.J_PRE_NAME}',
+                pool_size=5, max_overflow=10
+            )
+            logger.debug('Engines created')
+
+            try:
+                config_file = Path(__file__).parent / 'config.toml'
+                with config_file.open('rb') as config_file:
+                    cfg = tomllib.load(config_file)
+
+                self.OUTPUT_DIR = Path(Path(__file__).parent.parent / cfg['OUTPUT_DIR'])
+                if not self.OUTPUT_DIR.exists():
+                    self.OUTPUT_DIR.mkdir(parents=True)
+                    logger.info(f'Created output directory: {self.OUTPUT_DIR}')
+
+                self.REPORT_NAME = cfg['REPORT_NAME']
+                self.STORES = ', '.join([str(st) for st in cfg['STORES']])
+                self.START_DATE = cfg['DATETIME']['START_DATE']
+                self.END_DATE = cfg['DATETIME']['END_DATE']
+            except FileNotFoundError:
+                logger.exception(f'No configuration file found')
+                raise RuntimeError('No configuration file found, application cannot proceed.')
+            except Exception as e:
+                logger.exception(f'Error loading configuration file: {e}')
+                raise RuntimeError(f'Error loading configuration file: {e}')
+        else:
+            logger.error('No environment variables found')
+            raise RuntimeError('No .env file found, application cannot proceed.')
+
+
+settings = AppSettings()
